fc97bd28272554c4c424307fae70c1f31f0b948a
scottx611x <scottx611x@gmail.com>
2025-08-05 08:37:51 -0400

    fixup for now

--- pi/grip_server.py
+++ pi/grip_server.py
@@ -27,6 +27,7 @@ trace.set_tracer_provider(
 )
 span_processor = BatchSpanProcessor(OTLPSpanExporter(
     endpoint=os.getenv("OTEL_EXPORTER_OTLP_ENDPOINT"),
+    insecure=True
 ))
 trace.get_tracer_provider().add_span_processor(span_processor)
 
